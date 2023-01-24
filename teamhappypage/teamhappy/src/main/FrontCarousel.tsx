68e87430454d94dcee9a1d1c85245ab2fbfc4e12
karatejin <junsyang@naver.com>
2023-01-25 08:40:09 +0900

    2023-01-25"별점나오게 하기"

--- teamhappypage/teamhappy/src/main/FrontCarousel.tsx
+++ teamhappypage/teamhappy/src/main/FrontCarousel.tsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Slider, { CustomArrowProps } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+// import Box from '@mui/material/Box';
+import Rating, { RatingClasses } from '@mui/material/Rating';
 
 import Course1 from "../Asset/images/exwebfront.png";
 import Course2 from "../Asset/images/htmlbasic.png";
@@ -28,6 +30,12 @@ function PrevArrow(props: CustomArrowProps) {
   );
 }
 
+// 별점보기 수강평 등록후 평균 읽어오기
+function ReadOnly() {
+  const [value] = React.useState<number>(5);
+  <Rating name="read-only" value={value} readOnly />
+}
+
 
 export default class CustomArrows extends Component {
   render() {
@@ -39,7 +47,6 @@ export default class CustomArrows extends Component {
       nextArrow: <NextArrow />,
       prevArrow: <PrevArrow />,
     };
-
 
     return (
       <div className="course_dashboard4">
@@ -56,7 +63,7 @@ export default class CustomArrows extends Component {
                 <img src={Course1} alt="" className="course_face4" />
                 <div className="course_title4">강력한 CSS</div>
                 <div className="course_instructor4">asdf</div>
-                <div className="course_rating4"></div>
+                <div className="course_rating4"><Rating/></div>
                 <div className="course_price4">17000원</div>
                 <div className="course_tag4">베스트셀러</div>
               </div>
@@ -64,7 +71,7 @@ export default class CustomArrows extends Component {
                 <img src={Course2} alt="" className="course_face4" />
                 <div className="course_title4">React 완벽가이드</div>
                 <div className="course_instructor4">해피코딩</div>
-                <div className="course_rating4">4.7</div>
+                <div className="course_rating4"><Rating/></div>
                 <div className="course_price4">17000원</div>
                 <div className="course_tag4">베스트셀러</div>
               </div>
@@ -72,7 +79,7 @@ export default class CustomArrows extends Component {
                 <img src={Course1} alt="" className="course_face4" />
                 <div className="course_title4">React 완벽가이드</div>
                 <div className="course_instructor4">해피코딩</div>
-                <div className="course_rating4">4.7</div>
+                <div className="course_rating4"><Rating/></div>
                 <div className="course_price4">17000원</div>
                 <div className="course_tag4">베스트셀러</div>
               </div>
@@ -80,7 +87,7 @@ export default class CustomArrows extends Component {
                 <img src={Course1} alt="" className="course_face4" />
                 <div className="course_title4">React 완벽가이드</div>
                 <div className="course_instructor4">해피코딩</div>
-                <div className="course_rating4">4.7</div>
+                <div className="course_rating4"><Rating/></div>
                 <div className="course_price4">17000원</div>
                 <div className="course_tag4">베스트셀러</div>
               </div>
@@ -88,7 +95,7 @@ export default class CustomArrows extends Component {
                 <img src={Course1} alt="" className="course_face4" />
                 <div className="course_title4">React 완벽가이드</div>
                 <div className="course_instructor4">해피코딩</div>
-                <div className="course_rating4">4.7</div>
+                <div className="course_rating4"><Rating/></div>
                 <div className="course_price4">17000원</div>
                 <div className="course_tag4">베스트셀러</div>
               </div>
